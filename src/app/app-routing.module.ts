3be6b1411e32c3e76ba6a43b97c30ad3673a709e
JenoBenny <106422430+JenoBenny@users.noreply.github.com>
2023-08-27 23:30:35 +0530

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { AddContactComponent } from './add-contact/add-contact.component';
+import { ContactBookComponent } from './contact-book/contact-book.component';
+import {ContactDetailComponent} from './contact-detail/contact-detail.component'; 
+const routes: Routes = [
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'add-contact', component: AddContactComponent },
+  { path: 'contact-book', component: ContactBookComponent },
+  { path: 'contact/:name', component: ContactDetailComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
