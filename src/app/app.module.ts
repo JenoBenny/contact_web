695697a207e2773f8a570e87a925cd355469194e
JenoBenny <106422430+JenoBenny@users.noreply.github.com>
2023-08-27 23:30:04 +0530

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,28 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms'; // Import FormsModule
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { AddContactComponent } from './add-contact/add-contact.component';
+import { ContactBookComponent } from './contact-book/contact-book.component';
+import { ContactsService } from './contacts.service';
+import { ContactDetailComponent } from './contact-detail/contact-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    AddContactComponent,
+    ContactBookComponent,
+    ContactDetailComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule, // Add FormsModule here
     AppRoutingModule
   ],
-  providers: [],
+  providers: [ContactsService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
